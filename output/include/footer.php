fdf83939b9a6eb55322947a736b7c45130953593
mhernandez <mhernandez@dca.gob.gt>
2017-05-16 09:52:26 -0600

    modificado el inicio

--- output/include/footer.php
+++
@@ -1,4 +0,0 @@
-<footer class="container">
-© Derechos Reservados Diario de Centro América, 2015.<br>
-Casa Editora Tipografía Nacional 18 calle 6-72 zona 1.<br>
-Redacción PBX: 2414 9600 info@dca.gob.gt
